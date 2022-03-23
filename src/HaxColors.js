e473905b3f84c7e504dbaba475936538e2c0c5a8
Mike Wagner <mvw5669@psu.edu>
2022-03-22 22:02:44 -0400

    another try

--- src/HaxColors.js
+++ src/HaxColors.js
@@ -1,4 +1,4 @@
-import { html, css, LitElement } from 'lit';
+import { html, css, LitElement } from '../node_modules/lit';
 
 export class HaxColors extends LitElement {
   static get tag() {
--- src/HaxPainting.js
+++ src/HaxPainting.js
@@ -1,4 +1,4 @@
-import { html, css, LitElement } from 'lit';
+import { html, css, LitElement } from '../node_modules/lit';
 import './HaxCanvas.js';
 import './HaxColors.js';
 
