114e503a9aabe9f1631442182f7adaa4d318a17b
Mike Wagner <mvw5669@psu.edu>
2022-03-23 15:25:25 -0400

    cmon vercel

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,22 @@
+import nodeResolve from '@rollup/plugin-node-resolve';
+import html from '@web/rollup-plugin-html';
+import { importMetaAssets } from '@web/rollup-plugin-import-meta-assets';
+
 export default {
-	input: 'index.js',
+	input: 'index.html',
 	output: [
 		{
-			file: 'scripts.js',
-			format: 'iife'
+			format: 'es',
+            dir: 'dist'
 		}
-	]
+	],
+    plugins: [
+        html({
+            minify: true,
+            injectServiceWorker: process.env.VERCEL_ENV == "development" ? false : true,
+            serviceWorkerPath: 'dist/sw.js',
+        }),
+        nodeResolve(),
+        importMetaAssets()
+    ]
 };
