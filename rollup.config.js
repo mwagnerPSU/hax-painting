3c89a7167522f841fb1ae3f10a4f5ddf3ffd8f18
Mike Wagner <mvw5669@psu.edu>
2022-04-08 01:23:03 -0400

    local image support

--- rollup.config.js
+++ rollup.config.js
@@ -5,6 +5,7 @@ import { importMetaAssets } from '@web/rollup-plugin-import-meta-assets';
 import { terser } from 'rollup-plugin-terser';
 import { generateSW } from 'rollup-plugin-workbox';
 import path from 'path';
+import copy from 'rollup-plugin-copy';
 
 export default {
 	input: 'index.html',
@@ -19,6 +20,14 @@ export default {
             minify: true,
             injectServiceWorker: process.env.VERCEL_ENV == "development" ? false : true,
             serviceWorkerPath: 'dist/sw.js',
+        }),
+        copy({
+          targets: [
+            {
+              src: 'node_modules/@lrnwebcomponents/rpg-character/lib/**',
+              dest: 'dist/node_modules/@lrnwebcomponents/rpg-character/lib',
+            },
+          ],
         }),
         nodeResolve(),
         terser(),
